2f86fe77ae76c33b00bcd7c18daf7c96d1fc96df
HarryShapor <Hero1World23@gmail.com>
2026-04-14 17:42:08 +0300

    create project

---
+++ src/test/java/ru/shaporenko/intern/task_time_tracker/TaskTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shaporenko.intern.task_time_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTimeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
